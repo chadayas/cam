bc3c38e3e9acfb0c698681369cf154d8d101e404
chadayas <cgassoya@gmail.com>
2026-02-16 19:02:25 -0500

    camera_pins.h and camera_init func

--- main/cam.h
+++ main/cam.h
@@ -1,13 +1,8 @@
 #pragma once
 
-#include<stdio.h>
-#include<inttypes.h>
-#include<string>
-#include<iostream>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_system.h"
 #include "esp_http_server.h"
@@ -16,11 +11,10 @@
 #include "esp_netif.h"
 #include "nvs_flash.h"
 #include "esp_log.h"
-#include "esp_spiffs.h"
-#include "esp_adc/adc_oneshot.h"
-#include "esp_adc/adc_cali.h"
-#include "esp_adc/adc_cali_scheme.h"
+#include "esp_timer.h"
+#include "driver/gpio.h"
 #include "esp_camera.h"
+#include "camera_pins.h"
 
 #define TAG "[PROJECT]"
 
@@ -64,4 +58,7 @@ class Httpserver{
 		httpd_config_t cfg = HTTPD_DEFAULT_CONFIG();
 		httpd_handle_t svr = NULL;
 };
+
+
+esp_err_t init_camera();
 
